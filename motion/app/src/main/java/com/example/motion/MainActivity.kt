e404e2d5f505bd09d82c2cc10d7bf8f87f98898c
RyanChen <ryanchen2525@gmail.com>
2020-10-23 16:36:25 -0400

    animation advanced

---
+++ motion/app/src/main/java/com/example/motion/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.motion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * currently, one motionLayout can only support one transition;
+     * so this is the method to make multiple transition
+     * */
+}
